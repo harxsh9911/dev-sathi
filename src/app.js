66ae3f6261c78a1fac83dd01d97ebc2d993c0ac3
Harsh Dwivedi <harshdwivedi848@gmail.com>
2026-02-16 18:56:18 +0530

    multiple route handler

--- src/app.js
+++ src/app.js
@@ -2,18 +2,35 @@ const express = require("express");
 
 const app=express();
 
-app.use("/secret", (req,res) => {
-    res.send("hello from secret server");
-});
+// app.get("/user" ,(req,res)=>{
+//     res.send({firstname:"Horse", lastname:"pichai"});
+// });
+app.use("/user",(req,res,next)=>{
+    console.log("halding the route 1");
+    next();
+    
+},
+    (req, res,next)=>{
+        console.log("handling the route 2");
+        next();
+    },
+    (req,res,next)=>{
+    console.log("halding the route 3");
+    next();
+    
+},
+(req,res,next)=>{
+    console.log("halding the route 4");
+    res.send("response2!");
+}
+);
 
-app.use("/guki", (req,res) => {
-    res.send("hello from guki");
+app.post("/user", (req,res)=>{
+    res.send("data successfully saved in data base");
 });
-
-app.use("/maggie", (req,res) => {
-    res.send("hello from maggie");
+app.delete("/user",(req,res)=>{
+    res.send("data deleted successfully");
 });
-
 app.use((req,res) => {
     res.send("hello from server");
 });
