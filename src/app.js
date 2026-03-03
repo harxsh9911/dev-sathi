e71366ef22429c727c15afd27dc7f8e95c50e3a1
Harsh Dwivedi <harshdwivedi848@gmail.com>
2026-03-03 15:36:57 +0530

    encrypting the password

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 const express = require("express");
  const connectdb= require("./config/database");
+ const {validateSignUpData} = require("./utils/validation");
+ const bcrypt =require("bcrypt");
+ const validator = require('validator');
 const app=express();
 // used for schema
 const User =require("./model/user");
@@ -31,8 +34,23 @@ catch(err){
 })
 // this is a post request which save the request
 app.post("/signup", async (req, res)=>{
-        const user = new User(req.body);
+        
         try{
+            //validating of data
+            validateSignUpData(req);
+
+            const {firstName,lastName,email} =req.body;
+            // Encrypting password 
+            const { password } = req.body;
+            const passwordhash = await bcrypt.hash(password,10);
+            console.log(passwordhash);
+
+            const user = new User({
+                firstName,
+                lastName,
+                email,
+                password:passwordhash,
+            });
             await user.save();
             res.send("user is added");
         }
@@ -79,6 +97,36 @@ app.patch("/user/userId", async (req, res)=>{
             res.status(400).send("error"+err.message);
         }
         
+})
+
+app.post("/login", async(req, res)=>{
+    try{
+        const {email , password} =req.body;
+
+        if(!email || !validator.isEmail(email)){
+            throw new Error("Email is not valid");
+        }
+
+        if(!password){
+            throw new Error("Passwords is required");
+        }
+        const user = await User.findOne({email:email});
+        if(!user){
+            throw new Error("Email is not present");
+        }
+
+        const isPasswordValid= await bcrypt.compare(password,user.password);
+
+        if(isPasswordValid){
+            res.send("Login successfull");
+        }
+        else{
+            throw new Error("Password is not valid");
+        }
+    }
+    catch(err){
+        res.status(404).send("Error:"+err.message);
+    }
 })
 
 connectdb().then(()=>{
---
+++ src/utils/validation.js
@@ -0,0 +1,18 @@
+const validator = require('validator');
+
+const validateSignUpData =(req)=>{
+    const{firstName, lastName , email, password}=req.body;
+    if(!firstName || !lastName){
+        throw new Error("Name is not valid");
+    }
+    else if(!email || !validator.isEmail(email)){
+        throw new Error("Email is not valid");
+    }
+    else if(!validator.isStrongPassword(password)){
+        throw new Error("password is not strong");
+    }
+};
+
+module.exports = {
+    validateSignUpData,
+}
